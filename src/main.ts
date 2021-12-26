3e4ab08b8125eee34645feddde42ca2356bf43fe
saiumesh535 <d.sai535@gmail.com>
2021-12-26 11:39:46 +0530

    added path

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import {initAWS} from './s3'
 
 async function run(): Promise<void> {
   try {
-    const branchName = getInput('branch_name')
-    const bucketName = getInput('bucket_name')
-    const region = getInput('bucket_name')
+    const branchName = getInput('BRANCH_NAME')
+    const bucketName = getInput('BUCKET_NAME')
+    const region = getInput('REGION')
     const accessKeyId = getInput('AWS_ACCESS_KEY_ID')
     const secretAccessKey = getInput('AWS_SECRET_ACCESS_KEY')
     const configPath = getInput('CONFIG_PATH')
@@ -21,7 +21,7 @@ async function run(): Promise<void> {
     const config: BundleConfig[] = JSON.parse(
       readFileSync(configPath, {encoding: 'utf-8'})
     )
-    await checkBundle({bucket: bucketName, path: branchName}, config)
+    await checkBundle({bucket: `${bucketName}/${branchName}`, path: branchName}, config)
   } catch (error) {
     setFailed(error.message)
   }
